f394e74762272d7448759ca2a6e9a5ea7be9d7dc
JustLike420 <justlikevova@gmail.com>
2020-11-25 19:15:20 +0300

    navigation / post page

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,10 @@ from django.views.generic import ListView, DetailView
 def index(request):
     return render(request, 'index.html', {})
 
+
 def blog(request):
     return render(request, 'blog.html', {})
+
 
 def post(request):
     return render(request, 'post.html', {})
