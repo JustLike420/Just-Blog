85e3f4cb8988e393286dd909a9d2d7b9adf1ac8d
JustLike420 <justlikevova@gmail.com>
2020-11-25 17:29:56 +0300

    first step

--- Just_Blog/settings.py
+++ Just_Blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "blog/static",
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def blog(request):
+    return render(request, 'blog.html', {})
+
+def post(request):
+    return render(request, 'post.html', {})
