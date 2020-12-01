0a6599a450da6c482b29822b6717f849ed1b7d28
JustLike420 <justlikevova@gmail.com>
2020-12-02 00:54:26 +0300

    side bar categories / tags

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -8,3 +8,15 @@ register = template.Library()
 def get_popular(cnt=3):
     posts = Post.objects.order_by('-views')[:cnt]
     return {"posts": posts}
+
+
+@register.inclusion_tag('categories_posts_tpl.html')
+def get_cat():
+    categories = Category.objects.all()
+    return {"categories": categories}
+
+
+@register.inclusion_tag('tags_posts_tpl.html')
+def get_tag():
+    tags = Category.objects.all()
+    return {"tags": tags}
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, F
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -25,6 +26,19 @@ class Get_Posts(ListView):
     def get_context_data(self, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Blog'
+        return context
+
+
+class Get_Post(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
         return context
 
 
