56928df41b3ebbf65328e480165ceb4457284abc
JustLike420 <justlikevova@gmail.com>
2020-12-21 21:28:30 +0300

    small changes: footer/header/single post/tags.nav/models.author/main page/

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,10 @@ class Author(models.Model):
     def __str__(self):
         return self.user.username
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
 
 class Category(models.Model):
     title = models.CharField(max_length=255, verbose_name='Категория')
@@ -58,8 +62,6 @@ class Post(models.Model):
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
     category = models.ManyToManyField(Category, related_name='posts')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Тег')
-    previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
-    next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.title
