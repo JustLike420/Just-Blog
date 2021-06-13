6abe3a7c20441b79535cb5298617553416082455
JustLike420 <justlikevova@gmail.com>
2021-06-13 15:47:35 +0300

    postgresql added

--- Just_Blog/settings.py
+++ Just_Blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Just_Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Blog_BD',
+        'USER': 'postgres',
+        'PASSWORD': '13567vova957',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -101,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
